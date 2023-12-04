c85843a46ab7baaafd28a81097a8803609a0699d
Ramiz Raja <informramiz@gmail.com>
2023-12-04 20:45:44 +0500

    feat: Write class component

--- ShoppingApp/App.tsx
+++ ShoppingApp/App.tsx
@@ -5,12 +5,14 @@ import {
   View,
 } from 'react-native';
 import { Chair } from './Chair';
+import { Table } from './Table';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <View>
         <Chair/>
+        <Table/>
       </View>
     </SafeAreaView>
   );
---
+++ ShoppingApp/Table.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export class Table extends React.PureComponent {
+    render(): React.ReactNode {
+        return <Text>Table Class Component</Text>
+    }
+}

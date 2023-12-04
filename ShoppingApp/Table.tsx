fbba426ff7d08f2ccd9b3813d625b19681e245ac
Ramiz Raja <informramiz@gmail.com>
2023-12-04 21:12:51 +0500

    feat: Use state in class components

--- ShoppingApp/Table.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-export class Table extends React.PureComponent {
-    render(): React.ReactNode {
-        return <Text>Table Class Component</Text>
-    }
-}

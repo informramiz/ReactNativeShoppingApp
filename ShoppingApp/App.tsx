7e446bb5543a7ed51e85ea30b89ff665ab67fc87
Ramiz Raja <informramiz@gmail.com>
2023-12-05 18:51:09 +0500

    feat: Build basic splash screen

--- ShoppingApp/App.tsx
+++ ShoppingApp/App.tsx
@@ -1,30 +1,18 @@
 import React, { useState } from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  Text,
   View,
 } from 'react-native';
-import { Chair } from './Chair';
-import { Table } from './Table';
+import Splash from './src/screens/auth/Splash';
 
 function App(): JSX.Element {
-  const [theme, setTheme] = useState('light');
   return (
     <SafeAreaView>
       <View>
-        <Text style={styles.themeHeader} onPress={() => {setTheme(theme === 'light' ? 'dark' : 'light')}}>Toggle Theme</Text>
-        <Chair theme={theme}/>
-        <Table/>
+        <Splash/>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create(
-  {
-    themeHeader: {margin: 16, fontSize: 16, backgroundColor: 'yellow'}
-  }
-);
 
 export default App;
---
+++ ShoppingApp/src/screens/auth/Splash/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
+
+const Splash = () => {
+    return (
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')}/>
+            <Text style={styles.title}>You'll Find</Text>
+            <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
+            <Text style={styles.title}>Here!</Text>
+        </View>
+    )
+}
+
+export default Splash;

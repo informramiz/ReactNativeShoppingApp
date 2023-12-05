07305b4df7f180b49e742215f3035769706e8e85
Ramiz Raja <informramiz@gmail.com>
2023-12-05 17:15:30 +0500

    refactor: Change file to typescript

---
+++ ShoppingApp/Table.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "react-native";
+
+export class Table extends React.PureComponent<{}, {isBig: boolean}, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isBig: false
+        }
+    }
+
+    changeSizeState () {
+        console.log(this instanceof Table);
+        this.setState({isBig: !this.state.isBig})
+    }
+
+    render() {
+        return <Text style={{ fontSize: this.state.isBig ? 24 : 14}} onPress={this.changeSizeState.bind(this)}>Table Class Component</Text>
+    }
+}

0da032fd298187cec1bd9f3e55c41fc892294832
Ramiz Raja <informramiz@gmail.com>
2023-12-10 00:16:38 +0500

    feat: Integrate Google Sign in

--- ShoppingApp/App.tsx
+++ ShoppingApp/App.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   View,
 } from 'react-native';
-import Splash from './src/screens/auth/Splash';
 import Singup from './src/screens/auth/Signup';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from "react-native-config";
 
 function App(): JSX.Element {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: Config.GOOGLE_WEB_CLIENT_ID, // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      iosClientId: Config.GOOGLE_IOS_CLIENT_ID, // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  }, []);
+
   return (
     <SafeAreaView>
       <View>
--- ShoppingApp/src/components/GoogleLogin/index.js
+++ ShoppingApp/src/components/GoogleLogin/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { Image, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
 const GoogleLogin = ({ style }) => {
+    const handleGoogleLogin = async () => {
+        try {
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+          // setState({ userInfo });
+          console.log(userInfo);
+        } catch (error) {
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+          } else {
+            // some other error happened
+          }
+        }
+      };
+
     return (
-        <TouchableOpacity activeOpacity={0.6} style={[styles.container, style]}>
+        <TouchableOpacity activeOpacity={0.6} style={[styles.container, style]} onPress={handleGoogleLogin}>
             <Image style={styles.image} source={require('../../assets/google.png')}/>
         </TouchableOpacity>
     )

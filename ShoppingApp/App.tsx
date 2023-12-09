3f270b112c7b23f6399274aacd7d1fe0eb515699
Ramiz Raja <informramiz@gmail.com>
2023-12-10 02:30:13 +0500

    fix: App not working on Android

--- ShoppingApp/App.tsx
+++ ShoppingApp/App.tsx
@@ -46,7 +46,6 @@ function App(): JSX.Element {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
       webClientId: Config.GOOGLE_WEB_CLIENT_ID, // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
-      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
       forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
       iosClientId: Config.GOOGLE_IOS_CLIENT_ID, // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
     });

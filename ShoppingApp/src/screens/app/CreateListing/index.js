316d9cd6b4b0c9bc9f6b6148d672497063f21ac0
Ramiz Raja <informramiz@gmail.com>
2023-12-15 02:45:11 +0500

    feat: Complete image picker component for add new listing

--- ShoppingApp/src/screens/app/CreateListing/index.js
+++ ShoppingApp/src/screens/app/CreateListing/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ScrollView, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Image, Pressable, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import Header from "../../../components/Header";
 import { safeAreaStyleProvider } from "../../../utils/safeareahelper";
@@ -8,21 +8,45 @@ import {aunchImageLibrary, launchImageLibrary} from 'react-native-image-picker';
 
 
 const CreateListing = ({ navigation }) => {
+    const [images, setImages] = useState([]);
+
     const onBackPress = () => {
         navigation.goBack();
     }
 
     const uploadNewImage = async () => {
         const result = await launchImageLibrary();
+        
+        if (result?.assets?.length) {
+            setImages([...images, ...result?.assets]);
+        }
     }
 
+    const onDeleteImage = (image) => {
+        setImages(images.filter((img) => (img?.fileName !== image?.fileName)));
+    }
 
     return (
         <ScrollView style={[styles.container, safeAreaStyleProvider(useSafeAreaInsets())]}>
             <Header title={'Create a new listing'} showBack onBackPress={onBackPress}/>
-            <TouchableOpacity onPress={uploadNewImage}>
-                <Text>+</Text>
-            </TouchableOpacity>
+            <Text style={styles.sectionTitle}>Upload photos</Text>
+                <View style={styles.imageRow}>
+                    <TouchableOpacity onPress={uploadNewImage} style={styles.uploadImageActionContainer}>
+                        <View style={styles.uploadIconBackground}>
+                            <Text style={styles.uploadIcon}>+</Text>
+                        </View>
+                    </TouchableOpacity>
+                    
+                    { images.map(image => (
+                        <View style={styles.imageContainer}>
+                            <Image key={image?.fileName} source={{uri: image.uri}} style={styles.image}/>
+                            <Pressable hitSlop={20} onPress={() => onDeleteImage(image)}>
+                                <Image style={styles.deleteImageIcon} source={require('../../../assets/close.png')} />
+                            </Pressable>
+                        </View>
+                    ))}
+                    
+                </View>
         </ScrollView>
     );
 }
--- ShoppingApp/src/screens/app/CreateListing/styles.js
+++ ShoppingApp/src/screens/app/CreateListing/styles.js
@@ -1,7 +1,63 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
         margin: 20
+    },
+    sectionTitle: {
+        fontSize: 14,
+        color: colors.blue,
+        fontWeight: '500',
+        marginTop: 20
+    },
+    imageRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        gap: 10,
+        justifyContent: 'flex-start',
+        flex: 1,
+        marginTop: 10
+    },
+    uploadImageActionContainer: {
+        width: 90,
+        height: 90,
+        borderWidth: 1,
+        borderColor: colors.grey,
+        borderStyle: 'dashed',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+        marginTop: 8,
+    },
+    uploadIconBackground: {
+        width: 30,
+        height: 30,
+        backgroundColor: colors.lightGrey,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    uploadIcon: {
+        color: colors.white,
+        fontSize: 30,
+        marginTop: -4
+    },
+    imageContainer: {
+        flexDirection: 'row'
+    },
+    deleteImageIcon: {
+        width: 24,
+        height: 24,
+        position: 'absolute',
+        end: -8,
+        top: -8,
+        tintColor: colors.blue
+    },
+    image: {
+        width: 90,
+        height: 90,
+        borderRadius: 8,
     }
 });
